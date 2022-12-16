a010c5c786fa375721fac93a7be35b993e06bbc8
HenriqueDiulio <henriquediulio3@gmail.com>
2022-12-16 15:58:30 -0300

    Estudos Java

--- Exercicios_java/exercicios/src/fundamentos/PrimeroPrograma.java
+++
@@ -1,9 +0,0 @@
-package fundamentos;
-
-public class PrimeroPrograma {
-	public static void main(String[] args) {
-		
-		System.out.println("Olá mundo, esse é meu primeiro programa no Curso de Java da Udemy!"
-				+"\nNessa jornada espero adquirir muito conhecimento e reforçar os que eu ja possuo.");
-	}
-}
